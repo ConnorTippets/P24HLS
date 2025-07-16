fc829cbf4856689a807f723380e60386f35ca0c2
ConnorTippets <meiscool466@gmail.com>
2025-07-16 17:32:20 -0500

    Refactor reader class

--- reader.py
+++ reader.py
@@ -4,6 +4,10 @@ class Reader:
     def __init__(self, handle : typing.Union[io.BufferedReader, io.BytesIO]):
         self.handle = handle
     
+    def _interpret_as(self, amount : int, format):
+        raw_data = self.read_bytes(amount)
+        return struct.unpack(format, raw_data)[0]
+    
     def position(self) -> int:
         return self.handle.tell()
     
@@ -14,12 +18,25 @@ class Reader:
         return self.handle.read(amount)
     
     def read_int(self) -> int:
-        raw_data = self.read_bytes(4)
-        return struct.unpack("<i", raw_data)[0]
+        return self._interpret_as(4, "<i")
     
     def read_ushort(self) -> int:
-        raw_data = self.read_bytes(2)
-        return struct.unpack("<H", raw_data)[0]
+        return self._interpret_as(2, "<H")
+    
+    def read_uchar(self) -> int:
+        return self._interpret_as(2, "<B")
+    
+    def read_float(self) -> float:
+        return self._interpret_as(2, "<f")
+    
+    def read_vector(self) -> tuple[float, float, float]:
+        x = self.read_float()
+        y = self.read_float()
+        z = self.read_float()
+        return x, y, z
+    
+    def read_qangle(self) -> tuple[float, float, float]:
+        return self.read_vector()
     
     def skip_bytes(self, amount : int):
         self.handle.seek(amount, os.SEEK_CUR)
