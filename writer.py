87ce468cbd4b986551ad06bbc97c6e413c62e7a8
ConnorTippets <meiscool466@gmail.com>
2025-07-16 22:05:15 -0500

    Refactor writer class

--- writer.py
+++ writer.py
@@ -4,6 +4,10 @@ class Writer:
     def __init__(self, handle : typing.Union[io.BufferedWriter, io.BytesIO]):
         self.handle = handle
     
+    def _interpret_as(self, raw_data : typing.Any, format : str):
+        raw_bytes = struct.pack(format, raw_data)
+        self.write_bytes(raw_bytes)
+    
     def position(self) -> int:
         return self.handle.tell()
     
@@ -11,12 +15,10 @@ class Writer:
         self.handle.write(raw)
     
     def write_int(self, data : int):
-        raw_data = struct.pack("<i", data)
-        self.write_bytes(raw_data)
+        self._interpret_as(data, "<i")
     
     def write_ushort(self, data : int):
-        raw_data = struct.pack("<H", data)
-        self.write_bytes(raw_data)
+        self._interpret_as(data, "<H")
     
     def skip_bytes(self, amount : int):
         self.write_bytes(bytes(amount))
