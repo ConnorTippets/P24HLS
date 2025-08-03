ee8eb6a1c4b0062bdc39a793c72315b990312da4
ConnorTippets <meiscool466@gmail.com>
2025-08-02 23:49:21 -0500

    Write sprp version 6 instead of 5

--- main.py
+++ main.py
@@ -7,39 +7,53 @@ from gamelump import GameLumpConverter
 from reader import Reader
 from writer import Writer
 
+
 def main():
     if len(sys.argv) < 2:
         print("expected usage of tool:")
         print("main [fname].bsp")
-        
+
         exit(1)
-    
+
     fname = sys.argv[1]
     with open(fname, "rb") as handle:
         reader = Reader(handle)
         bsp = BSPReader(reader).read()
-    
+
     bsp.version = 20
-    
+
+    if not bsp.header.lump_headers[8].length:
+        new_header = bsp.header.lump_headers[53]
+        new_header.offset = bsp.calc_new_offset(8)
+
+        bsp.set_lump(8, bsp.lumps[53], new_header)
+
     gamelump_t = bsp.lumps[GAMELUMPS_ID]
     gamelump_offset = bsp.header.lump_headers[GAMELUMPS_ID].offset
     gamelump_ver, gamelump_data = astuple(gamelump_t)
-    
+
     new_gamelump_offset = bsp.calc_new_offset(GAMELUMPS_ID)
-    
+
     with io.BytesIO(gamelump_data) as handle_in:
         with io.BytesIO() as handle_out:
             reader = Reader(handle_in)
             writer = Writer(handle_out)
-            GameLumpConverter(gamelump_offset, new_gamelump_offset, 5, reader, writer).convert()
-            
+            GameLumpConverter(
+                gamelump_offset, new_gamelump_offset, 6, reader, writer
+            ).convert()
+
             gamelump_out = handle_out.getvalue()
-    
-    bsp.set_lump(GAMELUMPS_ID, Lump(gamelump_ver, gamelump_out), LumpHeader(new_gamelump_offset, len(gamelump_out), gamelump_ver, 0))
-    
+
+    bsp.set_lump(
+        GAMELUMPS_ID,
+        Lump(gamelump_ver, gamelump_out),
+        LumpHeader(new_gamelump_offset, len(gamelump_out), gamelump_ver, 0),
+    )
+
     with open(fname[:-4] + "_d.bsp", "wb") as handle:
         writer = Writer(handle)
         BSPWriter(writer).write(bsp)
+
 
 if __name__ == "__main__":
     main()
