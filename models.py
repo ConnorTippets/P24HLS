955187dd1ab191672145ae6b36d737f174575d1e
ConnorTippets <meiscool466@gmail.com>
2026-01-24 19:58:21 -0600

    feat: read BSP from bytesio

---
+++ models.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from io import BytesIO
+import struct
+
+
+@dataclass
+class BSPLump:
+    id: int
+    offset: int
+    version: int
+    fourcc: tuple[int, int, int, int]
+    data: bytes
+
+    @classmethod
+    def from_bytes(cls, reader: BytesIO, id: int) -> "BSPLump":
+        offset: int = struct.unpack("<i", reader.read(4))[0]
+        length: int = struct.unpack("<i", reader.read(4))[0]
+        ver: int = struct.unpack("<i", reader.read(4))[0]
+        fourcc: tuple[int, int, int, int] = struct.unpack("<BBBB", reader.read(4))
+
+        cur_pos = reader.tell()
+        reader.seek(offset)
+        data = reader.read(length)
+        reader.seek(cur_pos)
+
+        return cls(id, offset, ver, fourcc, data)
+
+
+@dataclass
+class BSP:
+    version: int
+    map_revision: int
+    lumps: list[BSPLump]
+
+    @classmethod
+    def from_bytes(cls, reader: BytesIO) -> "BSP | None":
+        if not reader.read(4) == b"VBSP":
+            return
+
+        version: int = struct.unpack("<i", reader.read(4))[0]
+
+        lumps: list[BSPLump] = []
+        for i in range(64):
+            lumps.append(BSPLump.from_bytes(reader, i))
+
+        map_revision: int = struct.unpack("<i", reader.read(4))[0]
+
+        return cls(version, map_revision, lumps)
